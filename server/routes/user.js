1ace47c4cad4397347a5aa79f6616c95609327a7
Liz Martins <elizabeth.martins5@gmail.com>
2023-07-27 13:17:54 -0400

    added register/login functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,7 +10,27 @@ router.get('/', (req, res) => res.send('OK!'))
 router.post('/register', async (req, res) => {
   const { username, password } = req.body
   const user = await UserModel.findOne({ username })
-  res.json(user)
+  if (user) {
+    return res.json({ message: 'User already exists!' })
+  }
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const newUser = new UserModel({ username, password: hashedPassword })
+  await newUser.save()
+  res.json({ message: 'User Registered Successfully!' })
+})
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  const user = await UserModel.findOne({ username })
+  if (!user) {
+    return res.json({ message: 'No such user!' })
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password)
+  if (!isPasswordValid) {
+    return res.json({ message: 'Username or Password is Incorrect!' })
+  }
+  const token = jwt.sign({ id: user._id }, 'secret')
+  res.json({ token, userID: user._id })
 })
 
 export { router as userRouter }
